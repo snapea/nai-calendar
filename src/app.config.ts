84752de2739ec004523a809277a4c1896eff9a81
daye99 <848403550@qq.com>
2021-08-13 19:13:05 +0800

    feature: mine page

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,14 @@
+/*
+ * @Author: wenzhenjin
+ * @Date: 2021-08-09 18:04:32
+ * @LastEditors: wenzhenjin
+ * @LastEditTime: 2021-08-13 17:13:45
+ * @FilePath: /taro-mini/src/app.config.ts
+ */
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/mine/mine',
   ],
   window: {
     backgroundTextStyle: 'light',
