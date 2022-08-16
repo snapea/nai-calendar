11c703d5ca7d00be08ad5c0d20da8b7c0eea3c8f
daye99 <848403550@qq.com>
2022-08-16 18:48:31 +0800

    feature:图标

--- src/pages/cost/cost.tsx
+++ src/pages/cost/cost.tsx
@@ -1,66 +1,64 @@
 import React from 'react'
 // import { useState, useEffect, useCallback } from "react";
-import { View, Text, Swiper, SwiperItem } from "@tarojs/components";
-// import Taro from "@tarojs/taro";
+import { View, Text, Swiper, SwiperItem, Image } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 // import { AtButton } from "taro-ui";
 
-import "./cost.scss";
+import './cost.scss'
 
 const Cost = () => {
-    return (
-        <View className='cost-container'>
-            <iconpark-icon name="bowl"></iconpark-icon>
-            {/* 输入框 */}
-            <View className='date-header'>
-                <View className='date-left'>2022年8月14日~21日</View>
-                <View className='title-right'>收支日历</View>
+  return (
+    <View className='cost-container'>
+      <View className='date-header'>
+        <View className='date-left'>2022年8月14日~21日</View>
+        <View className='title-right'>收支日历</View>
+      </View>
+      <Swiper className='swiper-cotainer'>
+        <SwiperItem>
+          <View className='swiper-title'>总支出</View>
+          <View className='swiper-cash'>￥30.00</View>
+          <View className='swiper-detail'>
+            <View className='swiper-list-item'>
+              <View className='swiper-detail-key'>总收入</View>
+              <View className='swiper-detail-value'>￥1000.00</View>
             </View>
-            <Swiper className='swiper-cotainer'>
-                <SwiperItem>
-                    <View className='swiper-title'>总支出</View>
-                    <View className='swiper-cash'>￥30.00</View>
-                    <View className='swiper-detail'>
-                        <View className='swiper-list-item'>
-                            <View className='swiper-detail-key'>总收入</View>
-                            <View className='swiper-detail-value'>￥1000.00</View>
-                        </View>
-                        <View className='swiper-list-item'>
-                            <View className='swiper-detail-key'>周结余</View>
-                            <View className='swiper-detail-value'>￥970.00</View>
-                        </View>
-                    </View>
-                </SwiperItem>
-                <SwiperItem>2</SwiperItem>
-                <SwiperItem>3</SwiperItem>
-            </Swiper>
-            <View className='center-content'>
-                <View className='date-day'>
-                    <View className='date'>08/15</View>
-                    <View className='day'>星期一</View>
-                </View>
-                <View className='pay-notes'>
-                    <View className='pay'>支出:￥30.00</View>
-                    <View className='income'>收入:￥1000.00</View>
-                </View>
+            <View className='swiper-list-item'>
+              <View className='swiper-detail-key'>周结余</View>
+              <View className='swiper-detail-value'>￥970.00</View>
             </View>
-            <View className='cost-list'>
-                <View className='cost-item'>
-                    <View className='cost-item-left'>
-                        <View className='cost-item-icon'></View>
-                        <View className='cost-item-title'>工资</View>
-                    </View>
-                    <View className='cost-right'>￥1000</View>
-                </View>
-                <View className='cost-item'>
-                    <View className='cost-item-left'>
-                        <View className='cost-item-icon'></View>
-                        <View className='cost-item-title'>购物</View>
-                    </View>
-                    <View className='cost-right'>-￥10</View>
-                </View>
-            </View>
+          </View>
+        </SwiperItem>
+        <SwiperItem>2</SwiperItem>
+        <SwiperItem>3</SwiperItem>
+      </Swiper>
+      <View className='center-content'>
+        <View className='date-day'>
+          <View className='date'>08/15</View>
+          <View className='day'>星期一</View>
         </View>
-    )
+        <View className='pay-notes'>
+          <View className='pay'>支出:￥30.00</View>
+          <View className='income'>收入:￥1000.00</View>
+        </View>
+      </View>
+      <View className='cost-list'>
+        <View className='cost-item'>
+          <View className='cost-item-left'>
+            <View className='cost-item-icon'></View>
+            <View className='cost-item-title'>工资</View>
+          </View>
+          <View className='cost-right'>￥1000</View>
+        </View>
+        <View className='cost-item'>
+          <View className='cost-item-left'>
+            <View className='cost-item-icon'></View>
+            <View className='cost-item-title'>购物</View>
+          </View>
+          <View className='cost-right'>-￥10</View>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default Cost
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,89 +5,87 @@
  * @LastEditTime: 2021-08-16 18:24:21
  * @FilePath: /taro-mini/src/pages/index/index.tsx
  */
-import { useState, useEffect, useCallback } from "react";
-import { View, Text, Swiper, SwiperItem } from "@tarojs/components";
-import Taro from "@tarojs/taro";
-import { AtButton } from "taro-ui";
+import { useState, useEffect, useCallback } from 'react'
+import { View, Text, Swiper, SwiperItem } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss"; // 按需引入
-import "./index.scss";
+import 'taro-ui/dist/style/components/button.scss' // 按需引入
+import './index.scss'
 import calendarUtil, {
   getCalendarEles,
-  getTimeFormat
-} from "../../utils/calendar";
+  getTimeFormat,
+} from '../../utils/calendar'
 
 const Calendar = () => {
-  const [calendarEles, setCalendarEles] = useState([]);
-  const [previousEles, setPreviousEles] = useState([]);
-  const [nextEles, setNextEles] = useState([]);
+  const [calendarEles, setCalendarEles] = useState([])
+  const [previousEles, setPreviousEles] = useState([])
+  const [nextEles, setNextEles] = useState([])
 
-  const [date, setDate] = useState("");
-  const [day, setDay] = useState(0);
-  const [swiperCurrent, setSwiperCurrent] = useState(0);
+  const [date, setDate] = useState('')
+  const [day, setDay] = useState(0)
+  const [swiperCurrent, setSwiperCurrent] = useState(0)
 
   // const [y, setY] = useState(getTimeFormat(new Date(), "year"));  // 2021
   // const [m, setM] = useState(getTimeFormat(new Date(), "month")); // 8
 
-  let y = getTimeFormat(new Date(), "year");
-  let m = getTimeFormat(new Date(), "month");
+  let y = getTimeFormat(new Date(), 'year')
+  let m = getTimeFormat(new Date(), 'month')
 
-  const monthIndex = m;
+  const monthIndex = m
 
   useEffect(() => {
     console.log(123123)
-    setCalendarEles(getCalendarEles(new Date()));
+    setCalendarEles(getCalendarEles(new Date()))
     setPreviousEles(
-      getCalendarEles(new Date().setMonth(new Date().getMonth() - 1))
-    );
-    setNextEles(
-      getCalendarEles(new Date().setMonth(new Date().getMonth() + 1))
-    );
-    setDay(getTimeFormat(new Date(), "day"));
-    setDate(y + "年" + m + "月");
-  }, []);
+      getCalendarEles(new Date().setMonth(new Date().getMonth() - 1)),
+    )
+    setNextEles(getCalendarEles(new Date().setMonth(new Date().getMonth() + 1)))
+    setDay(getTimeFormat(new Date(), 'day'))
+    setDate(y + '年' + m + '月')
+  }, [])
   const handleCalendarClick = useCallback(ev => {
-    console.log(ev);
-  }, []);
+    console.log(ev)
+  }, [])
   return (
-    <View className="calendar">
-      <View className="title">
-        <View className="date-title">{date}</View>
+    <View className='calendar'>
+      <View className='title'>
+        <View className='date-title'>{date}</View>
       </View>
-      <View className="calendar-header">
-        <View className="calendar-header-title">一</View>
-        <View className="calendar-header-title">二</View>
-        <View className="calendar-header-title">三</View>
-        <View className="calendar-header-title">四</View>
-        <View className="calendar-header-title">五</View>
-        <View className="calendar-header-title">六</View>
-        <View className="calendar-header-title">七</View>
+      <View className='calendar-header'>
+        <View className='calendar-header-title'>一</View>
+        <View className='calendar-header-title'>二</View>
+        <View className='calendar-header-title'>三</View>
+        <View className='calendar-header-title'>四</View>
+        <View className='calendar-header-title'>五</View>
+        <View className='calendar-header-title'>六</View>
+        <View className='calendar-header-title'>七</View>
       </View>
-      <View className="swiper-wrapper">
-        <View className="calendar-body">
-          {calendarEles.map((item:any, index) => {
+      <View className='swiper-wrapper'>
+        <View className='calendar-body'>
+          {calendarEles.map((item: any, index) => {
             return (
               // <View>1</View>
               <View
                 className={[
-                  "calendar-body-ele",
-                  index % 7 === 5 || index % 7 === 6 ? "vacation-active" : null,
+                  'calendar-body-ele',
+                  index % 7 === 5 || index % 7 === 6 ? 'vacation-active' : null,
                   item.solar === day ||
                   (item.lunar.IDayCn && item.lunar.IDayCn.length) === 3
-                    ? "taday"
-                    : ""
-                ].join(" ")}
+                    ? 'taday'
+                    : '',
+                ].join(' ')}
                 onClick={() => handleCalendarClick(item)}
               >
                 <View>{item.solar}</View>
-                <View className="solarText">{item.lunar.IDayCn}</View>
+                <View className='solarText'>{item.lunar.IDayCn}</View>
               </View>
-            );
+            )
           })}
         </View>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default Calendar;
+export default Calendar
